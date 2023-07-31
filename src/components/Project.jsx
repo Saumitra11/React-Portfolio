aef357fcead75988f0b34d23b0c2450cdfa2f8f6
Saumitra Pathak <saumitrapathak11@gmail.com>
2023-07-31 18:16:19 +0530

    Skills complete

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Project = () => {
   return (
-    <div>Project</div>
-  )
-}
+    <section id="works" className="py-10 text-white">
+      <div className="text-center">
+        <h3 className="text-4xl font-bold">
+          My <span className="text-cyan-600">Works</span>
+        </h3>
+        <p className="text-gray-400 mt-3 text-lg opacity-0">Works</p>
+      </div>
+      <br />
+      <div className="flex max-w-6xl px-5 mx-auto items-center relative">
+        <div className="lg:w-2/3 w-full">
+          <Swiper slidesPerView={1.2} spaceBetween={20} breakpoints={{}}>
+            <SwiperSlide>
+              <div className="h-fit w-full p-4 bg-slate-700 rounded-xl"></div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Project
+export default Project;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
+  const skills = [
+    {
+      logo: "code-outline",
+      language: "C++",
+      level: "Advance",
+      count: 90,
+    },
+    {
+      logo: "logo-javascript",
+      language: "JavaScript",
+      level: "Intermediate",
+      count: 65,
+    },
+    {
+      logo: "logo-react",
+      language: "React.js",
+      level: "Intermediate",
+      count: 50,
+    },
+    {
+      logo: "logo-python",
+      language: "Python",
+      level: "Beginner",
+      count: 32,
+    },
+  ];
   return (
     <section id="skills" className="py-10 bg-gray-800 relative">
-      <div className='mt-8 text-gray-100 text-center '>
-        <h3 className='text-4xl font-bold'>
-          My <span className='text-cyan-600'>Skills</span>
+      <div className="mt-8 text-gray-100 text-center ">
+        <h3 className="text-4xl font-bold">
+          My <span className="text-cyan-600">Skills</span>
         </h3>
-        <p className='text-gray-300 mt-3 text-lg opacity-0'>My Knowledge</p>
-        <div className='flex items-center '></div>
+        <p className="text-gray-300 mt-3 text-lg opacity-0">My Knowledge</p>
+        <div className="flex items-center justify-center mt-12 gap-10 flex-wrap">
+          {skills.map((skill, i) => (
+            <div
+              key={i}
+              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
+            >
+              <div
+                style={{
+                  background: `conic-gradient(rgb(8, 145, 170) ${skill.count}%, #ddd ${skill.count}%)`,
+                }}
+                className="w-32 h-32 flex items-center justify-center rounded-full"
+              >
+                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600">
+                  <ion-icon name={skill.logo}></ion-icon>
+                </div>
+              </div>
+              <p className="text-xl mt-3 ">{skill.language}</p>
+              {/* <p className="text-xl mt-3 ">{skill.level}</p> */}
+            </div>
+          ))}
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
