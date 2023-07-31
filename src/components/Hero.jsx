00ce4f3d177ce435be7c706cad74084f9fc2d1e5
Saumitra Pathak <saumitrapathak11@gmail.com>
2023-07-31 14:09:58 +0530

    Hero Section Almost Complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import me from "../assets/images/result.png";
 
 const Hero = () => {
+  const socialMedia = ["logo-linkedin", "logo-twitter"];
   return (
-    <div className='h-screen'>Hero</div>
-  )
-}
+    <section
+      id="portal"
+      className="min-h-screen flex py-10 md:flex-row flex-col items-center"
+    >
+      <div className="flex-1 flex items-center justify-center h-full">
+        <img
+          src={me}
+          alt=""
+          className="mx-20 md:w-6/12 md:mt-30 h-90 object-cover"
+        />
+      </div>
+      <div className="flex-1">
+        <div className="md:text-left text-center">
+          <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
+            <span className="text-cyan-600 md:text-6xl text-5xl">Hello!</span>
+            <br />I am <span>Saumitra Pathak </span>
+          </h1>
+          <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-600">
+            Software Engineer
+          </h4>
+          <button className="btn-primary mt-8">Connect With Me</button>
+          <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
+            {socialMedia.map((social, i) => (
+              <div
+                key={social}
+                className="text-gray-400 hover:text-white cursor-pointer"
+              >
+                <ion-icon name={social}></ion-icon>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       const nav = document.querySelector("nav");
@@ -42,11 +43,32 @@ const Navbar = () => {
             Sau<span className="text-cyan-600">mit</span>ra
           </h4>
         </div>
-        <div className="text-gray-300 md:block hidden px-7 py-2 font-medium bg-transparent">
+        <div className="text-gray-300 md:block hidden px-7 py-2 font-medium bg-gray-900 rounded-bl-full">
           <ul className="flex items-center gap-1 py-2 text-lg">
             {menuLinks?.map((menu, i) => (
               <li key={i} className="px-6 hover:text-cyan-600">
                 <a href={menu?.link}>{menu?.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`z-[998] ${
+            open ? "text-gray-900" : "text-gray-100"
+          } text-3xl md:hidden m-5 `}
+        >
+          <ion-icon name="menu"></ion-icon>
+        </div>
+        <div
+          className={`md:hidden text-gray-900 absolute w-1/3 h-screen px-7 py-2 font-medium bg-white top-0 duration-300 ${
+            open ? "right-0" : "right-[-100%]"
+          }`}
+        >
+          <ul className="flex flex-col justify-center h-full gap-10 py-2 text-lg">
+            {menuLinks.map((menu, i) => (
+              <li key={i} className="px-6 hover:text-cyan-600">
+                <a href={menu.link}>{menu.name}</a>
               </li>
             ))}
           </ul>
